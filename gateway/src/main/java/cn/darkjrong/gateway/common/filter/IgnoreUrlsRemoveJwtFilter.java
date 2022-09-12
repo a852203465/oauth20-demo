f4679edab12105e75dfb11ffc7ffaf40d3746be7
Rong.Jia <852203465@qq.com>
2022-09-12 11:14:15 +0800

    feature(server): oauth2 相关接口取消移除JWT请求头

--- gateway/src/main/java/cn/darkjrong/gateway/common/filter/IgnoreUrlsRemoveJwtFilter.java
+++ gateway/src/main/java/cn/darkjrong/gateway/common/filter/IgnoreUrlsRemoveJwtFilter.java
@@ -1,6 +1,8 @@
 package cn.darkjrong.gateway.common.filter;
 
 import cn.darkjrong.gateway.common.config.WhiteListConfig;
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
@@ -18,21 +20,27 @@ import java.util.List;
  * 白名单路径访问时需要移除JWT请求头
  * @author Rong.Jia
  */
+@Slf4j
 @Component
 public class IgnoreUrlsRemoveJwtFilter implements WebFilter {
+
+    private static final String AUTH_PREIFX = "oauth";
 
     @Autowired
     private WhiteListConfig whiteListConfig;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+
+        log.debug("-----IgnoreUrlsRemoveJwtFilter--------------");
+
         ServerHttpRequest request = exchange.getRequest();
         URI uri = request.getURI();
         PathMatcher pathMatcher = new AntPathMatcher();
         //白名单路径移除JWT请求头
         List<String> ignoreUrls = whiteListConfig.getUrls();
         for (String ignoreUrl : ignoreUrls) {
-            if (pathMatcher.match(ignoreUrl, uri.getPath())) {
+            if (!StrUtil.contains(uri.getPath(), AUTH_PREIFX) && pathMatcher.match(ignoreUrl, uri.getPath())) {
                 request = exchange.getRequest().mutate().header("Authorization", "").build();
                 exchange = exchange.mutate().request(request).build();
                 return chain.filter(exchange);
